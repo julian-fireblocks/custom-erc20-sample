ec8084842520a3bc92d2f597fae1f998ea2d5fe8
Ho Yih Xiang Julian <jho@fireblocks.com>
2025-07-30 11:51:11 +0800

    chore: add specific error codes

--- contracts/SimpleERC20.sol
+++ contracts/SimpleERC20.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 /// @title Simple ERC20 Token (Upgradeable)
 /// @notice A minimal ERC20 implementation for demonstration purposes, upgradeable version
-contract SimpleERC20 is Initializable, ContextUpgradeable, IERC20Upgradeable {
+contract SimpleERC20 is Initializable, ContextUpgradeable, IERC20 {
+    // Custom errors for revert codes
+    error ApproveFromZeroAddress();
+    error ApproveToZeroAddress();
+    error TransferFromZeroAddress();
+    error TransferToZeroAddress();
+    error TransferAmountExceedsBalance();
+    error MintToZeroAddress();
+    error TransferAmountExceedsAllowance();
     string private _name;
     string private _symbol;
     uint8 private _decimals;
@@ -64,31 +72,31 @@ contract SimpleERC20 is Initializable, ContextUpgradeable, IERC20Upgradeable {
 
     function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
         uint256 currentAllowance = _allowances[from][_msgSender()];
-        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        if (currentAllowance < amount) revert TransferAmountExceedsAllowance();
         _transfer(from, to, amount);
         _approve(from, _msgSender(), currentAllowance - amount);
         return true;
     }
 
     function _transfer(address from, address to, uint256 amount) internal {
-        require(from != address(0), "ERC20: transfer from the zero address");
-        require(to != address(0), "ERC20: transfer to the zero address");
-        require(_balances[from] >= amount, "ERC20: transfer amount exceeds balance");
+        if (from == address(0)) revert TransferFromZeroAddress();
+        if (to == address(0)) revert TransferToZeroAddress();
+        if (_balances[from] < amount) revert TransferAmountExceedsBalance();
         _balances[from] -= amount;
         _balances[to] += amount;
         emit Transfer(from, to, amount);
     }
 
     function _mint(address account, uint256 amount) internal {
-        require(account != address(0), "ERC20: mint to the zero address");
+        if (account == address(0)) revert MintToZeroAddress();
         _totalSupply += amount;
         _balances[account] += amount;
         emit Transfer(address(0), account, amount);
     }
 
     function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
+        if (owner == address(0)) revert ApproveFromZeroAddress();
+        if (spender == address(0)) revert ApproveToZeroAddress();
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
